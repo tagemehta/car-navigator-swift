da9156b584f75582e180597b2e3c07e65ad84b1b
TM64715 <tage.mehta@gmail.com>
2024-12-11 23:42:44 -0500

    UX Changes. Doesn't crash on lowercase. Gives some feedback on screen change and submit

--- YOLO/InputViewController.swift
+++ YOLO/InputViewController.swift
@@ -9,65 +9,91 @@ import UIKit
 
 class InputViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
-
-    @IBAction func goToMainScreen(_ sender: UIButton) {
-        performSegue(withIdentifier: "goToMain", sender: nil)
-    }
+    private let feedBackGenerator = UINotificationFeedbackGenerator()
+    private var args: (color: String?, model: String?, error: String?) = (color: nil, model: nil, error: nil)
+    private let ttsHelper = TextToSpeechHelper()
+    private let colorMapping: [String: String] = [
+        "white": "White",
+        "silver": "SilverGrey",
+        "grey": "SilverGrey",
+        "gray": "SilverGrey",
+        "black": "Black",
+        "brown": "Brown",
+        "red": "Red",
+        "maroon": "Red",
+        "orange": "Orange",
+        "yellow": "Yellow",
+        "gold": "Yellow",
+        "green": "Green",
+        "blue": "Blue",
+        "navy": "Blue",
+        "purple": "Purple",
+        "violet": "Purple"
+    ]
+    @IBOutlet weak var warningText: UILabel!
     
     override func viewDidLoad() {
         textField.delegate = self
+        feedBackGenerator.prepare()
+        warningText.isHidden = true
     }
     
-    let colorMapping: [String: String] = [
-        "White": "White",
-        "Silver": "SilverGrey",
-        "Grey": "SilverGrey",
-        "Gray": "SilverGrey",
-        "Black": "Black",
-        "Brown": "Brown",
-        "Red": "Red",
-        "Maroon": "Red",
-        "Orange": "Orange",
-        "Yellow": "Yellow",
-        "Gold": "Yellow",
-        "Green": "Green",
-        "Blue": "Blue",
-        "Navy": "Blue",
-        "Purple": "Purple",
-        "Violet": "Purple"
-    ]
+    override func viewDidAppear(_ animated: Bool) {
+        textField.delegate = self
+    }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "goToMain" {
-            if let destination = segue.destination as? ViewController {
-                if let text = textField.text, !text.isEmpty {
-                    if let result = parseCarDetails(text) {
-                        destination.carColorfilter = colorMapping[result.color]!
-                        destination.carMakeModelfilter = result.model
-                    } else {
-                        print("Invalid car details.")
-                    }
-                } else {
-                    print("Text field is empty or nil.")
-                }
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        var errorMsg = ""
+        if let text = textField.text, !text.isEmpty {
+            let newArgs = parseCarDetails(textField.text ?? "")
+            if newArgs.error == nil {
+                args = newArgs
+                feedBackGenerator.notificationOccurred(.success)
+                warningText.isHidden = true
+                return true
+            } else {
+                errorMsg = newArgs.error!
+                warningText.text = errorMsg
             }
+        } else {
+            errorMsg = "Please enter a description of the car"
+            warningText.text = errorMsg
+        }
+        warningText.isHidden = false
+        ttsHelper.speak(text: errorMsg)
+        feedBackGenerator.notificationOccurred(.error)
+        return false
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ViewController {
+            destination.carColorfilter = args.color!
+            destination.carMakeModelfilter = args.model!
+            print(args)
         }
     }
     
-    func parseCarDetails(_ carDetails: String) -> (color: String, model: String)? {
+    func parseCarDetails(_ carDetails: String) -> (color: String?, model: String?, error: String?) {
         // Split the input string into words
         let words = carDetails.split(separator: " ")
         
         // Ensure there are at least two words (color and model)
         guard words.count > 1 else {
-            return nil
+            return (nil, nil, "Please enter your description in the form of color space model")
         }
         
         // Extract the color (first word) and the model (remaining words)
-        let color = String(words[0])
-        let model = words[1...].joined(separator: " ")
-        
-        return (color, model)
+        let colorLocal = String(words[0])
+        let modelLocal = words[1...].joined(separator: " ").lowercased()
+        if let color = colorMapping[colorLocal.lowercased()] {
+            if (ModelConstants.modelValues.contains(modelLocal)) {
+                return (color: color, model: modelLocal, error: nil)
+            }
+            else {
+                return (color: nil, model: nil, error: "We can't find that model of car")
+            }
+        } else {
+            return (color: nil, model: nil, error: "We don't know that color")
+        }
     }
 }
 
