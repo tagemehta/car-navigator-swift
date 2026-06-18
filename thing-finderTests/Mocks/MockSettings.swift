ec627895ee7ca57837418904b4afadca44b1e693
TM64715 <tage.mehta@gmail.com>
2026-06-17 22:03:41 -0400

    Remove waitingPhraseCooldown from MockSettings test configurations

--- thing-finderTests/Mocks/MockSettings.swift
+++ thing-finderTests/Mocks/MockSettings.swift
@@ -22,7 +22,6 @@ enum TestSettings {
     settings.announceWaitingMessages = true
     settings.speechRepeatInterval = 6.0
     settings.speechChangeInterval = 4.0
-    settings.waitingPhraseCooldown = 10.0
     settings.directionLeftThreshold = 0.33
     settings.directionRightThreshold = 0.66
     return settings
@@ -54,7 +53,6 @@ enum TestSettings {
     let settings = makeDefault()
     settings.speechRepeatInterval = 0.1
     settings.speechChangeInterval = 0.1
-    settings.waitingPhraseCooldown = 0.1
     return settings
   }
 }
